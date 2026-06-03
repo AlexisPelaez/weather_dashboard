d5b38e7d8f85363a9023a66b1636efae1099747c
AlexisPelaez <apelaez3@cps.edu>
2026-06-02 19:33:17 -0500

    Update

--- app.py
+++ app.py
@@ -36,6 +36,29 @@ def dashboard():
     )
 
     response = requests.get(url).json()
+    emoji_map = {
+        0: "☀️",
+        1: "🌤️",
+        2: "⛅",
+        3: "☁️",
+        45: "🌫️",
+        48: "🌫️",
+        51: "🌦️",
+        53: "🌦️",
+        55: "🌧️",
+        61: "🌧️",
+        63: "🌧️",
+        65: "🌧️",
+        71: "❄️",
+        73: "❄️",
+        75: "❄️",
+        80: "🌧️",
+        81: "🌧️",
+        82: "🌧️",
+        95: "⛈️",
+        96: "⛈️",
+        99: "⛈️"
+    }
 
     return render_template(
         "index.html",
@@ -47,6 +70,7 @@ def dashboard():
         humidity=response["hourly"]["relativehumidity_2m"],
         wind=response["hourly"]["windspeed_10m"],
         daily=response["daily"],
+        emoji_map=emoji_map,
         city=city_name,
         lat=lat,
         lon=lon
